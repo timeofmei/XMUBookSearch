413725ea7202078c3e26b2aa9d3f1c249abef980
timeofmei <timeofmei@outlook.com>
2021-06-04 20:43:43 +0800

    add multi-thread support and imporve robust

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import ujson
 from flask import Flask, request
-from parse import searchBook, getBookInfo
+from parse import Book
 app = Flask(__name__)
 
 
 @app.route('/', methods=["POST"])
 def hello_world():
         reqData = ujson.loads(request.get_data().decode("utf-8"))
-        print(type(reqData))
         bookUrl = reqData.get("bookUrl")
-        if bookUrl is not None:
-            return ujson.dumps(getBookInfo(bookUrl), ensure_ascii=False).encode("utf-8")
-        else:
+        if bookUrl is None:
             title = reqData.get("title")
-            page = reqData.get("page")
-            publisher = reqData.get("publisher")
-            author = reqData.get("author")
-            isbn = reqData.get("isbn")
-            year = reqData.get("year")
-            doctype = reqData.get("doctype")
-            lang_code = reqData.get("lang_code")
-            sort = reqData.get("sort")
-            orderby = reqData.get("orderby")
-            onlylendable = reqData.get("onlyreadable")
-            resData = ujson.dumps(searchBook(title=title, page=page, publisher=publisher, author=author, isbn=isbn, year=year, doctype=doctype, lang_code=lang_code, sort=sort, orderby=orderby, onlylendable=onlylendable), ensure_ascii=False)
+            page = reqData.get("page") if reqData.get("page") else 1
+            publisher = reqData.get("publisher") if reqData.get("publisher") else ""
+            author = reqData.get("author") if reqData.get("author") else ""
+            isbn = reqData.get("isbn") if reqData.get("isbn") else ""
+            year = reqData.get("year") if reqData.get("year") else ""
+            doctype = reqData.get("doctype") if reqData.get("doctype") else "ALL"
+            lang_code = reqData.get("lang_code") if reqData.get("lang_code") else "ALL"
+            sort = reqData.get("sort") if reqData.get("sort") else "M_AUTHOR"
+            orderby = reqData.get("orderby") if reqData.get("orderby") else "ASC"
+            onlylendable = reqData.get("onlyreadable") if reqData.get("onlyreadable") else "no"
+            resData = ujson.dumps(Book(page=page).startSearchBook(title=title, publisher=publisher, author=author, isbn=isbn, year=year, doctype=doctype, lang_code=lang_code, sort=sort, orderby=orderby, onlylendable=onlylendable), ensure_ascii=False)
             return resData
+        else:
+            return ujson.dumps(Book().getBookInfo(bookUrl), ensure_ascii=False).encode("utf-8")
 
 
 if __name__ == "__main__":
--- parse.py
+++ parse.py
@@ -1,18 +1,17 @@
 import httpx
+import threading
 from lxml.etree import HTML
 from getAPT import getAuthor, getPT
 
-
-def searchBook(title, page="1", publisher="", author="", isbn="", year="", doctype="ALL", lang_code="ALL", sort="M_AUTHOR", orderby="ASC", onlylendable="no"):
-    searchUrl = "https://catalog.xmu.edu.cn/opac/openlink.php"
-    bookUrl = "https://catalog.xmu.edu.cn/opac/"
-    allRecord = {}
-    allRecord["recordNum"] = 0
-    allRecord["recordInfo"] = []
-    for i in range(5):
+class Book:
+    def __init__(self, page=1):
+        self.ls = []
+        self.page = page
+    def _searchBook(self, title, page, publisher, author, isbn, year, doctype, lang_code, sort, orderby, onlylendable):
+        searchUrl = "https://catalog.xmu.edu.cn/opac/openlink.php"
         params = {
             "title": title,
-            "page": str(i+int(page)),
+            "page": page,
             "publisher": publisher,
             "author": author,
             "isbn": isbn,
@@ -25,46 +24,62 @@ def searchBook(title, page="1", publisher="", author="", isbn="", year="", docty
         }
         searchResult = httpx.get(searchUrl, params=params)
         if searchResult.status_code != 200:
-            continue
+            return
         pageResult = HTML(searchResult.text)
         try:
-            allRecord["recordNum"] = int(pageResult.xpath("//*[@class='book_article']/div[1]/p/strong/text()")[0])
+            self.bookNum = int(pageResult.xpath("//*[@class='book_article']/div[1]/p/strong/text()")[0])
         except IndexError:
-            continue
+            self.bookNum = 0
         recordList = pageResult.xpath("//li[@class='book_list_info']")
         for record in recordList:
             bookDetail = {}
-            bookDetail["link"] = bookUrl + record.xpath("./h3/a/@href")[0]
+            bookDetail["link"] = searchUrl[:-12] + record.xpath("./h3/a/@href")[0]
             bookDetail["name"] = record.xpath("./h3/a//text()")[0].replace("/", "").strip()
             bookDetail["author"] = getAuthor(record.xpath("./p/text()")[1])
             bookDetail["press"], bookDetail["publishTime"] = getPT(record.xpath("./p/text()")[2], bookDetail["author"])
             bookDetail["copyNum"] = int(record.xpath("./p/span/text()")[0].strip()[-1])
             bookDetail["availableNum"] = int(record.xpath("./p/span/text()")[1].strip()[-1])
-            allRecord["recordInfo"].append(bookDetail)
-    return allRecord
+            self.ls.append(bookDetail)
+    
+    def startSearchBook(self, title, publisher, author, isbn, year, doctype, lang_code, sort, orderby, onlylendable):
+        threads = []
+        for i in range(self.page, self.page+20):
+            t = threading.Thread(target=self._searchBook, kwargs={"title": title, "page": i, "publisher": publisher, "author": author, "isbn": isbn, "year": year, "doctype": doctype, "lang_code": lang_code, "sort": sort, "orderby": orderby, "onlylendable": onlylendable})
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
+        self.ls.sort(key=lambda some: int(some["name"].split(".")[0]))
+        for i in range(len(self.ls)-1):
+            if self.ls[i]["name"].split(".")[0] == self.ls[i+1]["name"].split(".")[0]:
+                self.ls[i] = None
+        self.ls = [book for book in self.ls if book is not None]
+        return {"bookNum": self.bookNum, "recordInfo": self.ls}
 
 
-def getBookInfo(url):
-    bookInfo = {}
-    bookInfo["name"] = None
-    bookInfo["author"] = None
-    bookInfo["borrowInfo"] = []
-    bookResult = httpx.get(url)
-    if bookResult.status_code != 200:
-        return bookInfo
-    bookPage = HTML(bookResult.text)
-    try:
-        copies = bookPage.xpath("//*[@class='whitetext']")
-    except IndexError:
-        return bookInfo
-    bookInfo["name"] = bookPage.xpath("//*[@id='item_detail']/dl[1]/dd/a/text()")[0].replace("/", "").strip()
-    bookInfo["author"] = getAuthor(bookPage.xpath("//*[@id = 'item_detail']/dl[1]/dd/text()")[0])
-    for copy in copies:
-        borrowDetail = {}
-        borrowDetail["index"] = copy.xpath("./td[1]/text()")[0].strip()
-        borrowDetail["id"] = copy.xpath("./td[2]/text()")[0].strip()
-        borrowDetail["volume"] = copy.xpath("./td[3]/text()")[0].strip()
-        borrowDetail["location"] = copy.xpath("./td[4]/text()")[0].strip()
-        borrowDetail["state"] = copy.xpath("./td[5]//text()")[0].strip()
-        bookInfo["borrowInfo"].append(borrowDetail)
-    return bookInfo
+    def getBookInfo(self, url):
+        borrowInfo = []
+        bookResult = httpx.get(url)
+        if bookResult.status_code != 200:
+            return borrowInfo
+        bookPage = HTML(bookResult.text)
+        try:
+            copies = bookPage.xpath("//*[@class='whitetext']")
+        except IndexError:
+            return borrowInfo
+        try:
+            _ = copies[0].xpath("./td[1]/text()")[0].strip()
+        except:
+            return borrowInfo
+        for copy in copies:
+            borrowDetail = {}
+            try:
+                borrowDetail["index"] = copy.xpath("./td[1]/text()")[0].strip()
+                borrowDetail["id"] = copy.xpath("./td[2]/text()")[0].strip()
+                borrowDetail["volume"] = copy.xpath("./td[3]/text()")[0].strip()
+                borrowDetail["location"] = copy.xpath("./td[4]/text()")[0].strip()
+                borrowDetail["state"] = copy.xpath("./td[5]//text()")[0].strip()
+            except:
+                pass
+            borrowInfo.append(borrowDetail)
+        return borrowInfo
